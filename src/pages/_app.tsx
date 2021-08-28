78775e79388b9a95f669ef8a1d630f020e40a7be
sleduardo20 <sl.eduardo20@gmail.com>
2021-08-28 20:22:45 -0300

    create theme and add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
@@ -11,6 +12,11 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;600&family=Roboto:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
       <GlobalStyles />
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,45 @@
-export default {} as const;
+export default {
+  colors: {
+    blue: '#115D8C',
+    orange: '#F25D27',
+    title: '#123952',
+    text: '#617480',
+    complement: '#A0ACB3',
+    shape1: '#FFFFFF',
+    shape02: '#DCE2E6',
+    green: '#51B853',
+    blueLow: '#DDE9F0',
+    background: '#F5F8FA',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+    xxxlarge: '6.4rem',
+  },
+  font: {
+    family: {
+      barlow:
+        "Barlow, --apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      roboto:
+        "Roboto, --apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    },
+
+    ligth: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem',
+    },
+  },
+} as const;
