a861681f07f26ef89edc9d3aea66b0894df143b4
sleduardo20 <sl.eduardo20@gmail.com>
2021-08-31 18:30:15 -0300

    ⛏  chore: create mock data in Card Component

---
+++ src/components/Card/mock.ts
@@ -0,0 +1,22 @@
+export const mockCard = [
+  {
+    src: 'https://images.unsplash.com/photo-1545145879-71cf2f60c2a7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
+    city: 'Florianopolis',
+    places: 98,
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1531952106257-043446a3aed5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+    city: 'Blumenau',
+    places: 29,
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1545145879-71cf2f60c2a7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
+    city: 'Bombinhas',
+    places: 43,
+  },
+  {
+    src: 'https://c8.alamy.com/comp/K2RHG1/city-gateway-jaragua-do-sul-santa-catarina-brazil-K2RHG1.jpg',
+    city: 'Jaragua do Sul',
+    places: 18,
+  },
+];
