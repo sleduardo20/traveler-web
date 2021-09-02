38d3c0187e18a19626be27f7862def4fc7670a6b
sleduardo20 <sl.eduardo20@gmail.com>
2021-09-02 16:09:58 -0300

    🔨 feat: create constants Icons

---
+++ src/constants/Icons.tsx
@@ -0,0 +1,19 @@
+import { FiAlertCircle, FiArrowLeft, FiCoffee, FiSearch } from 'react-icons/fi';
+import { FaRegCalendarCheck } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import { MdMonochromePhotos } from 'react-icons/md';
+import { BsStar, BsStarFill } from 'react-icons/bs';
+import { IoLogoWhatsapp } from 'react-icons/io';
+import { GoComment } from 'react-icons/go';
+
+export const IconSearch = <FiSearch size={24} />;
+export const IconClose = <AiOutlineClose size={24} />;
+export const IconPhoto = <MdMonochromePhotos size={24} />;
+export const IconStarFill = <BsStarFill size={24} />;
+export const IconStar = <BsStar size={24} />;
+export const IconWhatsapp = <IoLogoWhatsapp size={24} />;
+export const IconCalendar = <FaRegCalendarCheck size={24} />;
+export const IconCoffe = <FiCoffee size={24} />;
+export const IconArrowLeft = <FiArrowLeft size={24} />;
+export const IconComment = <GoComment size={24} />;
+export const IconAlert = <FiAlertCircle size={24} />;
