4819034933dfe76bcf75008ca65df41b5fb9190f
sleduardo20 <sl.eduardo20@gmail.com>
2021-09-02 20:17:21 -0300

    ✨ feat: add aria label

--- src/constants/Icons.tsx
+++ src/constants/Icons.tsx
@@ -6,14 +6,34 @@ import { BsStar, BsStarFill } from 'react-icons/bs';
 import { IoLogoWhatsapp } from 'react-icons/io';
 import { GoComment } from 'react-icons/go';
 
-export const IconSearch = <FiSearch size={24} />;
-export const IconClose = <AiOutlineClose size={24} />;
-export const IconPhoto = <MdMonochromePhotos size={24} />;
-export const IconStarFill = <BsStarFill size={24} />;
-export const IconStar = <BsStar size={24} />;
-export const IconWhatsapp = <IoLogoWhatsapp size={24} />;
-export const IconCalendar = <FaRegCalendarCheck size={24} />;
-export const IconCoffe = <FiCoffee size={24} />;
-export const IconArrowLeft = <FiArrowLeft size={24} />;
-export const IconComment = <GoComment size={24} />;
-export const IconAlert = <FiAlertCircle size={24} />;
+export const IconSearch = <FiSearch aria-label="icon-search" size={24} />;
+
+export const IconClose = <AiOutlineClose aria-label="icon-close" size={24} />;
+
+export const IconPhoto = (
+  <MdMonochromePhotos aria-label="icon-photo" size={24} />
+);
+
+export const IconStarFill = (
+  <BsStarFill aria-label="icon-star-fill" size={24} />
+);
+
+export const IconStar = <BsStar aria-label="icon-star " size={24} />;
+
+export const IconWhatsapp = (
+  <IoLogoWhatsapp aria-label="icon-whatsapp " size={24} />
+);
+
+export const IconCalendar = (
+  <FaRegCalendarCheck aria-label="icon-calendar " size={24} />
+);
+
+export const IconCoffe = <FiCoffee aria-label="icon-coffe " size={24} />;
+
+export const IconArrowLeft = (
+  <FiArrowLeft aria-label="icon-arrow-left " size={24} />
+);
+
+export const IconComment = <GoComment aria-label="icon-comment " size={24} />;
+
+export const IconAlert = <FiAlertCircle aria-label="icon-alert " size={24} />;
