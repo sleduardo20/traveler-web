78680d032e2fd1ec2f447230652cce273eca4568
sleduardo20 <sl.eduardo20@gmail.com>
2021-09-04 21:05:48 -0300

    ✨ feat: create ShowCase component

---
+++ src/components/ShowCase/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { IconAlert } from '../../constants/Icons';
+import * as S from './styles';
+
+export type ShowCaseProps = {
+  title: string;
+  description: string;
+  src: string;
+  detach?: boolean;
+};
+
+export const ShowCase = ({
+  title,
+  description,
+  src,
+  detach,
+}: ShowCaseProps) => {
+  return (
+    <S.Container>
+      <S.Info>
+        {detach && (
+          <span>
+            {IconAlert}
+            Destaque
+          </span>
+        )}
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </S.Info>
+      <Image src={src} alt={title} layout="fill" objectFit="cover" />
+      <S.Image />
+    </S.Container>
+  );
+};
