8a859958cc2a171fa17a943b00ab5a99b1c0bce1
sleduardo20 <sl.eduardo20@gmail.com>
2021-09-05 17:43:48 -0300

    🔧 chore: create data mock

---
+++ src/components/ShowCase/mockShowCase.ts
@@ -0,0 +1,6 @@
+export const mockShowCase = {
+  src: 'https://cdnstatic8.com/viagensecaminhos.com/wp-content/uploads/2013/08/vista-aerea-praia-dos-ingleses-florianopolis.jpg',
+  title: 'Praia dos Ingleses',
+  description:
+    'Uma parte do paraíso na terra. Frequentemente com águas claras em tons verdes e azuis. Um dos locais mais preferidos por turistas e viajantes.',
+};
